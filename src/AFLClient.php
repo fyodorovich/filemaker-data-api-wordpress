1f1c45c4b131664938a0e2f84ec140f38e2d435c
Malcolm Fitzgerald <git@businessdatasystems.co.nz>
2021-01-18 16:00:58 +1300

    ADD: public variables holding hard-coded layout targets and field names

--- src/AFLClient.php
+++ src/AFLClient.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Created by Netbeans.
+ * Created by Apache Netbeans
  * User: Malcolm Fitzgerald
  * Date: 2021/01/18
  * Time: 14:25
@@ -13,6 +13,37 @@ use \WP_Http;
 use \Exception;
 
 class AFLClient {
+
+    /**
+     * Name of FMP layout to target for client data
+     * @var String
+     */
+    public $clientLayout = 'WebData_Client';
+    /**
+     * Names of FMP fields to access for client data
+     * @var array
+     */
+    public $clientFields = ['id_client','email','Contracts::Firstname','Contracts::Firstname','Contracts::Surname','Contracts::Address','Contracts::City','Contracts::getFoundCount'];
+    /**
+     * Name of FMP layout to target for contract data
+     * @var String
+     */
+    public $contractLayout = 'WebData_Contract';
+      /**
+     * Names of FMP fields to access for contract data
+     * @var array
+     */
+    public $contractFields = ['Contract','Client','Inst. Due','Arrears','Arr. Charges','Transactions','Total Due'];
+        /**
+     * Name of FMP layout to target for transaction data
+     * @var String
+     */
+    public $transactionLayout = 'WebData_Transactions';
+        /**
+     * Names of FMP fields to access for transaction data
+     * @var array
+     */
+    public $transactionFields = ['Contract','Date','TX Code','DRCR','Amount','Details','Balance','Debit','Credit'];
 
     public function __construct() {
         $this->setBaseURL($settings->getServer(), $settings->getDatabase());
@@ -36,7 +67,7 @@ class AFLClient {
 
     /**
      * Generate the query string required to obtain Client details from FileMaker Pro
-     * @return empty string|array|Exception
+     * @return empty string','array','Exception
      */
     public function client_query() {
 
