2fc66664f8d65566a5b52cd1e1bc3687d278e47f
Malcolm Fitzgerald <git@businessdatasystems.co.nz>
2021-01-21 21:30:30 +1300

    TWEAK: add instruction to reload page when/if we fail to get data

--- src/ShortCodeTable.php
+++ src/ShortCodeTable.php
@@ -50,7 +50,7 @@ class ShortCodeTable extends ShortCodeBase
 
             return $this->generateTable($records, $attr);
         } catch (Exception $e) {
-            return 'Unable to load records.';
+            return 'Unable to load records. Please refresh/reload this page.';
         }
     }
 
