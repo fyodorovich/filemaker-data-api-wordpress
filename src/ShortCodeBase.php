94aad25a81b64c63cbe9319c167044081beb0ed8
Malcolm Fitzgerald <git@businessdatasystems.co.nz>
2021-01-22 13:18:08 +1300

    CHANGE: new function that provide user feedback.
    Allows us to standardise our response from all forms

--- src/ShortCodeBase.php
+++ src/ShortCodeBase.php
@@ -1,36 +1,32 @@
 <?php
 
-
 namespace FMDataAPI;
 
+class ShortCodeBase {
 
-class ShortCodeBase
-{
     /** @var FileMakerDataAPI */
     protected $api;
 
     /** @var Settings */
     protected $settings;
 
-    public function __construct(FileMakerDataAPI $api, Settings $settings)
-    {
+    public function __construct(FileMakerDataAPI $api, Settings $settings) {
         $this->api = $api;
         $this->settings = $settings;
     }
 
-    protected function outputField(array $record, $field, $type = null, $link = false)
-    {
-        if(!array_key_exists($field, $record)) {
+    protected function outputField(array $record, $field, $type = null, $link = false) {
+        if (!array_key_exists($field, $record)) {
             return '';
         }
 
-        switch(substr(strtolower($type), 0, 5)) {
+        switch (substr(strtolower($type), 0, 5)) {
             case 'image':
             case 'thumb':
                 $content = $this->sizeImage($type, $record[$field]);
                 break;
             case 'curre':
-                if(empty($record[$field])) {
+                if (empty($record[$field])) {
                     $content = '';
                     $content = (money_format('%#10n', 0));
                 } else {
@@ -42,34 +38,29 @@ class ShortCodeBase
                 $content = nl2br($record[$field]);
         }
 
-        if($link) {
+        if ($link) {
             return sprintf('<a href="%s">%s</a>', $link, $content);
         }
 
         return $content;
     }
 
-    protected function sizeImage($type, $path)
-    {
+    protected function sizeImage($type, $path) {
         $params = explode('-', $type);
-        $width = 'image' == $params[0]
-            ? (isset($params[1]) ? $params[1] : '')
-            : (isset($params[1]) ? $params[1] : '50');
+        $width = 'image' == $params[0] ? (isset($params[1]) ? $params[1] : '') : (isset($params[1]) ? $params[1] : '50');
 
         return sprintf('<img src="%s" width="%s" />', $path, $width);
     }
 
-
-    protected function validateAttributesOrExit(array $reqs, array $attr)
-    {
+    protected function validateAttributesOrExit(array $reqs, array $attr) {
         $err = [];
-        foreach($reqs as $req) {
-            if(!array_key_exists($req, $attr)) {
+        foreach ($reqs as $req) {
+            if (!array_key_exists($req, $attr)) {
                 $err[] = $req;
             }
         }
 
-        if(count($err)) {
+        if (count($err)) {
             print(sprintf('Error required attribute%s %s missing.', 1 == count($err) ? '' : 's', implode(',', $err)));
 
             return false;
@@ -98,10 +89,10 @@ class ShortCodeBase
      * @param type $field
      * @return string
      */
-        protected function formatCurrency($field, $returnZero = false) {
+    protected function formatCurrency($field, $returnZero = false) {
 
-        if (empty($field) || 0 == $field ) {
-            $content = $returnZero ? '0.00': '';
+        if (empty($field) || 0 == $field) {
+            $content = $returnZero ? '0.00' : '';
         } else {
             if (class_exists('NumberFormatter')) {
                 $fmt = new \NumberFormatter($this->settings->getLocale(), NumberFormatter::CURRENCY);
@@ -113,5 +104,12 @@ class ShortCodeBase
         return $content;
     }
 
-
+    protected function connectionError($reload = false) {
+        if ($reload) {
+            return 'Unable to load records. Please reload the page.';
+        } else {
+            return '<div class="error-alert">That\'s embarrassing!<br>We cannot locate any contract information for your account. Please contact Adelphi finance<br>Phone: <a href="tel:043852502">04 385 2502</a> or email <a href="mailto:info@adelphi.co.nz">info@adelphi.co.nz</a></div>';
         }
+    }
+
+}
